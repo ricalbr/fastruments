d82324e7be808e94023a0ed376e2beada7238828
paolomaran <paolo.maran@polimi.it>
2026-06-02 09:36:21 +0200

    QLASS: Sequence mode implemented, added current_mode property, added idle() function

--- fastruments/QLASSdriver.py
+++ fastruments/QLASSdriver.py
@@ -59,6 +59,7 @@ class CurrentDriver(Instrument):
         self.sleep = 0.010
         self.connect()
 
+
     def connect(self) -> None:
         """
         Establish connection with the instrument using specific serial parameters.
@@ -80,6 +81,8 @@ class CurrentDriver(Instrument):
             self.ranges = {0:2.77,
                            1:25.0,
                            2:47.72}
+            self._sequences = np.full((16,8,2),fill_value=-1,dtype=int)
+            self._current_mode = 'constant'
             # Common terminators for serial interfaces
             self.inst.read_termination = "\n\r"
             self.inst.write_termination = "\n\r"
@@ -408,6 +411,286 @@ class CurrentDriver(Instrument):
         #     except Exception:
         #         R.append(None)
         # return R
+
+
+    # ------------------------------------------------------------------
+    # Sequences mode implementation
+    # ------------------------------------------------------------------
+    
+    @property
+    def current_mode(self) -> str:
+        '''
+        Returns a string dictating the currently active mode:
+        - 'constant' for static regime (default when starting up, after idling, or after calling $t0)
+        - 'sequence' for dynamic regime (after calling $t1)
+        '''
+        return self._current_mode
+
+
+    @current_mode.setter
+    def current_mode(self,val: str) -> bool:
+        '''
+        Sets current mode to val.
+        'timing' functions as alias of 'sequence', but will print a warning.
+        'idle' will call the idle function, setting the _current_mode to 'constant' in the process.
+        
+        Raises
+        ------
+            ValueError: if provided value is not 'constant','sequence','timing', or 'idle'
+            TypeError: if provided value is not a string
+        '''
+        valid_str = ['constant','idle','sequence','timing']
+        if not isinstance(val,str):
+            try:
+                raise TypeError(f'[QLASS][ERROR] Provided current mode {val} must be a string, is {type(val)}.')
+            except:
+                raise TypeError(f'[QLASS][ERROR] Provided current mode is not a string and not convertible to one, is {type(val)}.')
+        if val not in valid_str:
+            raise ValueError(f'[QLASS][ERROR] Provided current mode {val} is not among valid current modes.')
+        
+        if val == 'constant':
+            self.start_constant_mode()
+        if val == 'sequence':
+            self.start_sequence_mode()
+        if val == 'idle':
+            self.idle()
+        if val == 'timing':
+            self.start_sequence_mode()
+            print('[QLASS][WARNING] Setting current_mode to sequences after current_mode = \'timing\' was called.')
+
+        return
+    
+
+    @property
+    def sequences(self) -> Sequence:
+        '''
+        Multidimensional array for indirect access to the _sequences array.
+        Has shape (16,8,2):
+        - 16 channels
+        - 8 time steps
+        - [0] for step duration, [1] for DAC value applied
+
+        Example
+        -------
+        To obtain the time duration of all steps:
+        sequences[:,:,0]
+        To obtain the DAC values applied by pin 7:
+        sequences[7,:,1]
+        To see the first step duration of all channels:
+        sequences[:,0,1]
+        '''
+        out = self._sequences.copy()
+        out.setflags(write=False)
+        return out
+
+    
+    def set_zero(self) -> None:
+        '''
+        Sets manually the current to all channels to zero.
+        '''
+        for i in range(16):
+            self.set_current_level(i,0)
+
+
+    def start_constant_mode(self) -> None:
+        '''
+        Start application of the saved values to the DACs in a constant 
+        manner.
+        '''
+        self.inst.write('$t0')
+        self._current_mode = 'constant'
+
+
+    def start_sequence_mode(self) -> None:
+        '''
+        Start application of the saved sequences. 
+        TODO testing
+        '''
+        if -1 in self._sequences:
+            print('[QLASS][WARNING] Uninitialized sequence elements detected.')
+        self.inst.write('$t1')
+        self.flush_serial()
+        self._current_mode = 'sequence'
+    
+
+    def idle(self) -> None:
+        '''
+        Sets the board in an idle state: constant mode + zero current at DACs.
+        '''
+        self.set_zero()
+        self.start_constant_mode()
+        self._current_mode = 'constant'
+
+
+    @property
+    def sequences(self):
+        '''
+        Multidimensional array for indirect access to the _sequences array.
+        Has shape (16,8,2):
+        - 16 channels
+        - 8 time steps
+        - [0] for step duration, [1] for DAC value applied
+
+        This property does not have a setter element, but its value must be modified by using the methods
+        set_sequence_element, set_sequence_array, and set_all_sequences. Via direct access, this property
+        is set to read only, and so will probably raise an error if tried to be modified directly.
+
+        Example
+        -------
+        To obtain the time duration of all steps:
+        sequences[:,:,0]
+        To obtain the DAC values applied by pin 7:
+        sequences[7,:,1]
+        To see the first step duration of all channels:
+        sequences[:,0,1]
+        '''
+        out = self._sequences.copy()
+        out.setflags(write=False)
+        return out
+    
+
+    def set_sequence_element(self,ch:int,pos:int,time:int,val:int):
+        '''
+        Sets a single sequence element for a given pin.
+        For traceability, also modifies the array _sequences.
+
+        Parameters
+        ----------
+        ch : int
+            The channel whose sequence needs to be changed. Goes from 0 to 15.
+        pos : int
+            The position in the sequence to change. Goes from 0 to 7.
+        time : int
+            Duration of this sequence step, in steps of 500 us.
+            Goes from 0 (500 us) to 999 (500 ms).
+        val : int
+            Value to apply to the DAC for this step.
+            As all DAC values, goes from 0 to 65535.
+        '''
+
+        if self._current_mode == 'sequence':
+            print('[QLASS][WARNING] Tried to modify sequences while in sequence mode. Idling.')
+            self.idle()
+            
+
+        try:
+            ch = int(ch)
+            pos = int(pos)
+            time = int(time)
+            val = int(val)
+        except ValueError:
+            raise ValueError('[QLASS][ERRROR] Invalid input values of set_sequence_element:\n'
+                            f'ch = {ch}, pos = {pos}, time = {time}, val = {val}')
+        except TypeError:
+            raise TypeError('[QLASS][ERRROR] Invalid input type of set_sequence_element:\n'
+                            f'ch = {type(ch)}, pos = {type(pos)}, time = {type(time)}, val = {type(val)}')
+
+        # input sanity check
+        if ch < 0 or ch >= 16:
+            raise ValueError(f'[QLASS][ERROR] ch = {ch} in set_sequence_element({ch},{pos},{time},{val}): must be between 0 and 15')
+        if pos < 0 or pos >= 8:
+            raise ValueError(f'[QLASS][ERROR] pos = {pos} in set_sequence_element({ch},{pos},{time},{val}): must be between 0 and 7')
+        if time < 0 or time >= 1000:
+            raise ValueError(f'[QLASS][ERROR] time = {time} in set_sequence_element({ch},{pos},{time},{val}): must be between 0 and 999')
+        if val < 0 or val >= 65536:
+            raise ValueError(f'[QLASS][ERROR] val = {val} in set_sequence_element({ch},{pos},{time},{val}): must be between 0 and 65535')
+        
+        #send command to board
+        res = self.inst.query(f'$s{ch:02d},{pos:01d},{time:03d},{val}')
+        exp_res = f'ch{ch:02d} [step{pos:01d}] for {time:03d} cycles = {val}'
+        # TODO check whether self.update is needed here
+        
+        if res.strip() != exp_res:
+            self._sequences[ch,pos,0] = -1
+            self._sequences[ch,pos,1] = -1
+            raise RuntimeError(f'[QLASS][ERROR] Failed sequence element writing: set_sequence_element({ch},{pos},{time},{val}) -> {res}')
+
+        #update sequences array
+        self._sequences[ch,pos,0] = time
+        self._sequences[ch,pos,1] = val
+
+        if self.verbose:
+            print(f'[QLASS] Element set for channel {ch}, position {pos}: val = {val} for {time} steps')
+        return
+    
+    
+    def set_sequence_array(self,ch,time_arr,val_arr):
+        '''
+        Sets the sequence for a whole channel.
+        time_arr and val_arr must be 8 entries-long numpy arrays.
+
+        Parameters
+        ----------
+        ch : int
+            The channel whose sequence needs to be changed. Goes from 0 to 15.
+        time_arr : List of int
+            Duration of all sequence steps, in steps of 500 us.
+            Goes from 0 (500 us) to 999 (500 ms).
+            Must have shape (8,)
+        val_arr : List of int
+            Values to apply to the DAC for all steps.
+            As all DAC values, goes from 0 to 65535.
+            Must have shape (8,)
+        '''
+        # input sanity checks
+        time_arr = np.squeeze(time_arr)
+        val_arr = np.squeeze(val_arr)
+        if time_arr.shape != (8,):
+            raise ValueError(f'[QLASS][ERROR] Invalid shape for time_arr: {time_arr.shape} (must be (8,))')
+        if val_arr.shape != (8,):
+            raise ValueError(f'[QLASS][ERROR] Invalid shape for val_arr: {val_arr.shape} (must be (8,))')
+        
+        for i, (t, v) in enumerate(zip(time_arr, val_arr)):
+            self.set_sequence_element(ch, i, t, v)
+        
+        return
+    
+    
+    def set_all_sequences(self,time_seqs,val_seqs):
+        '''
+        Sets the complete sequences to the memory of the board.
+        time_seqs and val_seqs must be (16,8) matrices
+
+        Parameters
+        ----------
+        time_arr : List of list of int
+            Duration of all sequence steps, in steps of 500 us.
+            Goes from 0 (500 us) to 999 (500 ms).
+            Must have shape (16,8)
+        val_arr : List of list of int
+            Values to apply to the DACs for all steps.
+            As all DAC values, goes from 0 to 65535.
+            Must have shape (16,8)
+        '''
+
+        time_seqs = np.squeeze(time_seqs)
+        val_seqs = np.squeeze(val_seqs)
+
+        if time_seqs.shape != (16,8):
+            raise ValueError(f'[QLASS][ERROR] Invalid shape for time_seqs: {time_seqs.shape} (must be (16,8))')
+        if val_seqs.shape != (16,8):
+            raise ValueError(f'[QLASS][ERROR] Invalid shape for val_seqs: {val_seqs.shape} (must be (16,8))')
+        
+        for i in range(16):
+            self.set_sequence_array(i,time_seqs[i],val_seqs[i])
+        return
+    
+
+    def initialize_sequences(self):
+        '''
+        Sets the values of all elements in the sequence to 0 mA for 500 ms.
+        '''
+        was_verbose = self.verbose
+        if was_verbose:
+            print('[QLASS] Initializing sequences to zero.')
+            self.verbose = False
+
+        try:
+            self.set_all_sequences(time_seqs = np.full((16,8),fill_value=999,dtype=int), val_seqs = np.zeros((16,8),dtype=int))
+        finally:
+            self.verbose = was_verbose
+
+        return
 
 
 
